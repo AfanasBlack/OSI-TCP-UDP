3ff2d246f7747c097144a803b8a9415f05080773
Alexandr Afanasev <116384401+AfanasBlack@users.noreply.github.com>
2023-01-28 19:28:49 +0300

    Task 2

--- src/main/java/Client.java
+++ src/main/java/Client.java
@@ -4,6 +4,7 @@ import java.io.InputStreamReader;
 import java.io.PrintWriter;
 import java.net.Socket;
 import java.net.UnknownHostException;
+import java.util.Scanner;
 
 public class Client {
 
@@ -11,15 +12,26 @@ public class Client {
     private static String ip = "localhost";
 
     public static void main(String[] args) {
+        System.out.println("Игра в города: ");
+
         try (Socket clientSocket = new Socket(ip, PORT);
              PrintWriter out = new PrintWriter(clientSocket.getOutputStream(), true);
              BufferedReader in = new BufferedReader(new InputStreamReader(clientSocket.getInputStream()));){
 
-            out.println("Alexandr");
+            String currentCity = in.readLine();
+            Scanner scanner = new Scanner(System.in);
 
-            String serverResult = in.readLine();
+            if (currentCity.equals("???")) {
+                System.out.println("Введиде город с которого начнется игра: ");
+                out.println(scanner.nextLine());
+                System.out.println(in.readLine());
+            } else {
+                System.out.println("Город: " + currentCity);
+                System.out.println("Введите город оканчивающийся на букву: " + currentCity.toCharArray()[currentCity.length() - 1]);
 
-            System.out.println("Server response: " + serverResult);
+                out.println(scanner.nextLine());
+                System.out.println(in.readLine());
+            }
 
         } catch (IOException e) {
             throw new RuntimeException(e);
--- src/main/java/Server.java
+++ src/main/java/Server.java
@@ -8,21 +8,42 @@ import java.net.Socket;
 public class Server {
 
     private static int PORT = 8080;
+    private static String currentCity = "???";
 
     public static void main(String[] args) {
         try (ServerSocket serverSocket = new ServerSocket(PORT);) {
 
             System.out.println("Server started! ");
 
-            try (Socket clientSocket = serverSocket.accept();
-                 PrintWriter out = new PrintWriter(clientSocket.getOutputStream(), true);
-                 BufferedReader in = new BufferedReader(new InputStreamReader(clientSocket.getInputStream()));)
-            {
-                System.out.println("New connection accepted: " + clientSocket.getInetAddress());
-                final String name = in.readLine();
-                out.println(String.format("Hi %s, your port is %d", name, clientSocket.getPort()));
-            }
+            while (true) {
 
+                try (Socket clientSocket = serverSocket.accept();
+                     PrintWriter out = new PrintWriter(clientSocket.getOutputStream(), true);
+                     BufferedReader in = new BufferedReader(new InputStreamReader(clientSocket.getInputStream()));) {
+
+                    System.out.println("New connection accepted: " + clientSocket.getInetAddress());
+                    out.println(currentCity);
+                    System.out.println("Текущий город: " + currentCity);
+
+                    String cityClient = in.readLine();
+                    cityClient = cityClient.toLowerCase();
+
+                    if (currentCity.equals("???")) {
+                        currentCity = cityClient;
+                        out.println("OK");
+                    } else {
+                        char lastChar = currentCity.charAt(currentCity.length() - 1);
+                        char firstChar = cityClient.charAt(0);
+
+                        if (lastChar == firstChar) {
+                            currentCity = cityClient;
+                            out.println("OK");
+                        } else {
+                            out.println("NOT OK");
+                        }
+                    }
+                }
+            }
         } catch (IOException e) {
             throw new RuntimeException(e);
         }
